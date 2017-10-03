188363b792e89599142057c0ffce008d5b1f88e7
Geisy Domiciano <geisydomiciano@gmail.com>
2017-10-03 15:43:21 -0300

    add responsive header

--- components/FixedHeader.vue
+++ components/FixedHeader.vue
@@ -1,10 +1,10 @@
 <template>
     <header class="Header">
-        <h1 class="Header-brand">
-            <img class="Header-brand--logo" src="~/static/icon.png" alt="logo: github octocat" />
+        <img class="Header-brand--logo" src="~/static/icon.png" alt="logo: github octocat" />
+        <h1 class="Header-brand--title">
             Contributor's
-            <img class="Header-brand--image" src="~/assets/img/chart.png" alt="image of a bar chart, the bar colors are blue, red and green from left to right on increase order" />
         </h1>
+        <img class="Header-brand--image" src="~/assets/img/chart.png" alt="image of a bar chart, the bar colors are blue, red and green from left to right on increase order" />
     </header>
 </template>
 
@@ -25,12 +25,56 @@
         background: url('~/assets/img/github-chart.png') center center repeat;
     }
 
-    .Header-brand {
-
-    }
-
-    [class^="Header-brand"] {
+    [class^="Header-brand--"] {
         height: 50px;
         vertical-align: middle;
+        display: inline-block;
+        margin-right: 15px;
     }
+
+    .Header-brand--title {
+        font-size: 26px;
+        line-height: 50px;
+    }
+
+    [class^="Header-brand--"]:last-child {
+        margin: 0;
+    }
+
+    @media screen and (min-width: 768px) {
+
+        .Header {
+            height: 80px;
+            padding: 10px;
+        }
+
+        [class^="Header-brand--"] {
+            height: 60px;
+        }
+
+        .Header-brand--title {
+            font-size: 38px;
+        line-height: 60px;
+        }
+    }
+
+
+    @media screen and (min-width: 1024px) {
+
+        .Header {
+            height: 100px;
+            padding: 10px;
+        }
+
+        [class^="Header-brand--"] {
+            height: 80px;
+        }
+
+        .Header-brand--title {
+            font-size: 46px;
+            line-height: 80px;
+        }
+    }
+
+
 </style>
