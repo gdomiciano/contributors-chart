cd46f3723d85923d3c23a9e7ccd56d61a8a80afb
Geisy Domiciano <geisydomiciano@gmail.com>
2017-10-02 22:53:21 -0300

    add error component

---
+++ components/Error.vue
@@ -0,0 +1,21 @@
+<template>
+    <p class="Error-msg">{{message}}</p>
+</template>
+
+<script>
+    export default {
+        name: 'Error',
+
+        props: {
+            message: {
+                type: String,
+                required: true,
+            },
+        },
+
+    };
+</script>
+
+<style scoped>
+
+</style>
