0b10a3daa260f2eb0a0d1619bcfb7c5ff76d0c41
Geisy Domiciano <geisydomiciano@gmail.com>
2018-11-12 23:07:13 +0100

    :white_check_mark: add ui tests

---
+++ cypress/integration/page_load.spec.js
@@ -0,0 +1,29 @@
+describe('Page Load', () => {
+  before(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('should load header content', () => {
+    cy.get('[data-cy=header]')
+      .should('be.visible')
+    cy.get('[data-cy=logo]')
+      .should('have.class', 'Header-brand--logo')
+      .and('be.visible')
+    cy.get('[data-cy=title]')
+      .should('be.visible')
+      .and('not.be.empty')
+    cy.get('[data-cy=image]')
+      .should('be.visible')
+  })
+
+  it('should load initial content', () => {
+    cy.get('[data-cy=instruction]')
+      .should('be.visible')
+      .and('not.be.empty')
+    cy.get('[data-cy=search-field]')
+      .should('be.visible')
+      .and('be.empty')
+      .and('to.be.enabled')
+      .and('have.attr', 'placeholder')
+  })
+})
