7a914dfa77cf818ddf05fcc0d375de38f8c9dbcb
Mariia Vitiuk <mariiavitiuk@DWMAC-C02XPE4EJG5J.local>
2019-09-23 10:25:13 +0200

    remove extra spec file

--- cypress/integration/search_typeahead.spec.js
+++ cypress/integration/search_typeahead.spec.js
@@ -1,7 +1,10 @@
 describe('Search-TypeAhead', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:57403')
   })
+
+  const user = 'm'
+  const repo = 'wp-calypso'
 
   it('should search & return repos from store', () => {
     const user = 'nuxt-community'
@@ -12,6 +15,22 @@ describe('Search-TypeAhead', () => {
 
     cy.getPropfromStore('repoList').should('be.an', 'Array').and('not.be.empty')
 
-    cy.get('[data-cy=repo-item]').its('length').should('to.be.greaterThan', 0)
+    cy.get('[data-cy=repo-item]').its('length')
+      .should('to.be.greaterThan', 0)
+  })
+
+  it('assert number of contributors is 0', () => {
+    cy.get('[data-cy=search-field').type(user)
+
+    cy.makeStubbedRequest(`https://api.github.com/repos/${user}/${repo}/contributors`, 'fx:two-contributors.json', 'contributors')
+
+    cy.get('[data-cy=repo-suggestions]').within(() => {
+      cy.get('[data-cy=repo-item]')
+        .contains(`${user}/${repo}`).click()
+    })
+
+    cy.wait('@contributors').its('responseBody').should(($response) => {
+      expect($response).to.have.length(2)
+    })
   })
 })
