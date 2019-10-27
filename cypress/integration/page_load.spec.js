5fe96c559d95a897cae968cbb5df790067f66cab
Geisy Domiciano <geisydomiciano@gmail.com>
2019-10-27 22:39:05 +0100

    :bookmark::rocket::art: improve translations

--- cypress/integration/page_load.spec.js
+++ cypress/integration/page_load.spec.js
@@ -7,29 +7,25 @@ describe('Page Load', () => {
   })
 
   ALL_VIEWPORTS.forEach((viewPort) => {
+
     it(`${viewPort} should load header content`, () => {
-      cy.viewport(viewPort)
-      cy.get('[data-cy=header]')
-        .should('be.visible')
-      cy.get('[data-cy=logo]')
-        .should('have.class', 'Header-brand--logo')
-        .and('be.visible')
-      cy.get('[data-cy=title]')
-        .should('be.visible')
-        .and('not.be.empty')
-      cy.get('[data-cy=image]')
-        .should('be.visible')
+
+      cy.viewport(viewPort, 'landscape')
+
+      cy.get('[data-cy=header]').should('be.visible')
+      cy.get('[data-cy=logo]').should('have.class', 'Header-brand--logo').and('be.visible')
+      cy.get('[data-cy=title]').should('be.visible').and('not.be.empty')
+      cy.get('[data-cy=image]').should('be.visible')
     })
 
     it(`${viewPort} should load initial main content`, () => {
+
       cy.viewport(viewPort)
-      cy.get('[data-cy=instruction]')
-        .should('be.visible')
-        .and('not.be.empty')
-      cy.get('[data-cy=search-field]')
-        .should('be.visible')
-        .and('be.empty')
+
+      cy.get('[data-cy=instruction]').should('be.visible').and('not.be.empty')
+      cy.get('[data-cy=search-field]').should('be.visible')
         .and('be.enabled')
+        .and('be.empty')
         .and('have.attr', 'placeholder', 'GitHub username')
     })
   })
